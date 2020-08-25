8245048f9675e64c340a4e7bbf126e125b02ab79
Madhu Bhavanam <madhu_bhavanam@comcast.com>
2020-08-25 06:44:00 -0400

    second commit

---
+++ src/com/exmp/Example2.java
@@ -0,0 +1,10 @@
+package com.exmp;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is another Main Example");
+
+	}
+
+}
--- src/com/test/Example.java
+++ src/com/test/Example.java
@@ -4,6 +4,8 @@ public class Example {
 
 	public static void main(String[] args) {
 		System.out.println("This is Main Example");
+		System.out.println("This is Main Example 2");
+		System.out.println("This is Main Example 3");
 
 	}
 
